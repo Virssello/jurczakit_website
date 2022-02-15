2c5392ad8ab9b15c820f668be6d364a00215a634
Virssello <sebo3j.sj@gmail.com>
2022-02-16 00:42:54 +0100

    Text updates

---
+++ components/Footer.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Footer = () => (
+    <>
+    <div className="footer"></div>
+    © Sebastian Jurczak. All rights reserved.
+    </>
+    );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,23 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-export const Header=(
-)=>(<>
+export const Header = () => (
+  <>
     <div className="header">
-            <h1>
-            <Image src="/logoJurczakIt.png" alt="jurczakit Logo" width={150} height={150} />
-                Jurczak.it
-            </h1>
-        </div>
-        <div className="menu">
-            <Link href="/">Home</Link>
-            <Link href="/contact">Contact</Link>
-        </div>
-</>
-)
+      <h1>
+        <Image
+          src="/logoJurczakIt.png"
+          alt="jurczakit Logo"
+          width={150}
+          height={150}
+        />
+        Jurczak.it
+      </h1>
+    </div>
+    <div className="menu">
+      <Link href="/">Home</Link>
+      <Link href="/contact">Contact</Link>
+    </div>
+  </>
+);
